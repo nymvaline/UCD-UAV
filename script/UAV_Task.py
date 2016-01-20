729f1ce0bac3152535cbcae3f92b7e7760a8a6d8
khjtony <khjtony@gmail.com>
2016-01-20 15:48:55 -0800

    begin working on datalogger class and sensor API classes

--- script/UAV_Task.py
+++ script/UAV_Task.py
@@ -10,6 +10,7 @@ import mavros.command
 import mavros_msgs.msg
 import mavros_msgs.srv
 import time
+import pyserial
 from datetime import datetime
 
 class vector3(object):
@@ -143,26 +144,60 @@ class API_XBee(object):
     '''
     This is the interface to the XBee via simple transparency grammer
     '''
+    def __init__(self, PAN, ID):
+        self.pan = PAN
+        self.ID = ID
+    def read(self):
+        pass
+    def _setID(self):
+        pass
+    def _setTargetID(self):
+        pass
+    def _serialize(self):
+        pass
+    def _deserialize(self):
+        pass
     pass
 
 class API_AirTemp(API_XBee):
     '''
     This is the interface to air temperature in ground data logger
     '''
+    def read(self):
+        pass
+    def _serialize(self):
+        pass
+    def _deserialize(self):
+        pass
     pass
 
 class API_SoilTemp(API_XBee):
     '''
     This is the interface to Soil moisture sensor from ground data logger
     '''
-
+    def read(self):
+        pass
+    def _serialize(self):
+        pass
+    def _deserialize(self):
+        pass
     pass
 
 class API_Moisture(API_XBee):
     '''
     This is interface to soil moisture sensor from  ground data logger
     '''
+    def read(self):
+        pass
+    def _serialize(self):
+        pass
+    def _deserialize(self):
+        pass
     pass
 
-class Task_GetData(API_AirTemp, API_Moisture, API_SoilTemp):
+class Task_GetData(object):
+    def __init__(self):
+        pass
+
+
     pass
